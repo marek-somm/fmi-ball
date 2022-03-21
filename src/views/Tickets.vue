ab841c2e0ba98d8b59247edfbbef838dbfdd2558
Marek Sommerfeld <marek1999@online.de>
2022-03-21 21:35:46 +0100

    Update Tickets.vue

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -4,20 +4,25 @@
 			<div class="header">
 				<h2 class="title">Kartenverkauf und Abholung</h2>
 				<p class="subtitle">
-					Momentan werden noch keine Karten verkauft<br />Der Kartenverkauf
-					öffnet bald
+					Momentan sind noch keine Karten erhältlich<br />Die
+					Kartenreservierung öffnet bald
 				</p>
 
-				<FancyButton v-if="false" link="" text="Jetzt reservieren" @click="" />
+				<FancyButton
+					v-if="false"
+					link=""
+					text="Jetzt reservieren"
+					@click=""
+				/>
 			</div>
 			<div class="content">
 				<p>
-					Trotz einer Online-Reservierung müssen die Karten abgeholt
-					werden.
+					Sie können ihre Karten Online reservieren. Diese müssen dann
+					jedoch später in anderen Verkaufsstellen wie z.B. ausgewählten
+					Mensen erworben werden.
 				</p>
 				<p>
-					Nähere Informationen dazu sind in der Bestätigungs-Email
-					nachzulesen.
+					Nähere Informationen folgen in Kürze.
 				</p>
 			</div>
 		</div>
